dd28abd1d611ce1d960b9fa6a9a3d36d906b19ac
Michael-M-M <53363813+Michael-M-M@users.noreply.github.com>
2022-04-05 12:54:45 +0300

    abcd

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
-#include<stdio.h>  
-void main()  
-{
-  printf("0-whatsmyname.c");  
-  getch();  
+const char *filename = argv[2];
+FILE* output_file;
+
+output_file = fopen(filename, "wb");
+//validate file pointer
+if(!output_file) {
+   perror("Unable to open file for writing.");
+   if (argc > 2 && filename) {
+     fprintf(stderr, "\"%s\"\n", filename);
+   }
+   return EXIT_FAILURE;
 }
